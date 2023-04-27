867675fbc30b9e537d241524a255e6a3b8a2bea0
Francisco Medina <medinafran2002@hotmail.com>
2023-04-27 15:22:15 -0300

    coneccion cliente

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+FORMAT = 'UTF-8'
+HOST = '192.168.1.12'    # The remote host
+PORT = 8989             # The same port as used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        s.send(input('Accion: ').encode(FORMAT))
+
